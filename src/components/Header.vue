e6e2e67027091600df493393a6692493a951a2cb
Guilherme Welter <guilherme.welter@blueticket.com.br>
2018-04-18 16:51:08 -0300

    initial

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand"><a>Stock Trader</a></router-link>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link class="nav-item" activeClass="active" tag="li" to="/portfolio"><a class="nav-link">Portfolio</a></router-link>
+        <router-link class="nav-item" activeClass="active" tag="li" to="/stocks"><a class="nav-link">Stocks</a></router-link>
+      </ul>
+      <ul class="nav navbar-nav narbar-right">
+        <li class="nav-item"><a href="#" class="nav-link">End Day</a></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Save & Load
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save</a>
+            <a class="dropdown-item" href="#">Load</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
