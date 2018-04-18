f5b51912274460d6928857ca17d16237a957c339
Guilherme Welter <guilherme.welter@blueticket.com.br>
2018-04-18 18:45:29 -0300

    Criando stocks

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,7 @@ export default {
 </script>
 
 <style>
+  body {
+    padding: 30px;
+  }
 </style>
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>The Stocks/Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title text-success">
+          {{stock.name}}
+          <small>(Price: {{stock.price}})</small>
+        </h3>
+      </div>
+      <div class="card-body">
+        <div>
+          <input type="number" class="form-control" v-model="quantity" min="0">
+          <button
+            class="btn btn-success btn-block"
+            @click="buyStock"
+            :disabled="quantity <= 0">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['stock'],
+  data () {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    buyStock () {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+      console.log(order)
+      this.quantity = 0
+    }
+  }
+}
+</script>
+
